4b978d5d708b69f3e0cf739da774945ea4a09491
EC2 Default User <ec2-user@ip-172-31-17-60.ap-northeast-1.compute.internal>
2023-05-05 05:19:38 +0000

    [Add]配送先登録機能

--- app/controllers/public/items_controller.rb
+++ app/controllers/public/items_controller.rb
@@ -1,7 +1,26 @@
 class Public::ItemsController < ApplicationController
   def index
   end
+  
+  def new
+    @item = Item.new
+  end
+  
+  def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to admin_item_path(@item.id)
+    else
+      render :new
+    end
+  end
 
   def show
+  end
+  
+  private
+  
+  def item_params
+    params.require(:item).permit(:name, :introduction, :price, :is_active)
   end
 end
