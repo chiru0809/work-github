c505a53b4c77b9f0a1e4fd51349ee88d45352fea
EC2 Default User <ec2-user@ip-172-31-17-60.ap-northeast-1.compute.internal>
2023-05-16 04:45:39 +0000

    [Add]注文情報入力画面

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -4,6 +4,19 @@ class Public::OrdersController < ApplicationController
 
   def new
     @order = Order.new
+    @address = Address.new
+  end
+  
+  def confirm
+    @order = Order.new(order_params)
+    @address = Address.find(params[:order][:address_id])
+    @order.postal_code = current_customer.postal_code
+    @order.address = current_customer.address
+    @order.name = current_customer.first_name + current_customer.last_name
+    @cart_items = current_customer.cart_items
+    @total_price = 0
+    @shipping_cost = 800
+    @total_amount = (@total_price + @shipping_cost)
   end
   
   def create
@@ -25,7 +38,6 @@ class Public::OrdersController < ApplicationController
   private
   
   def order_params
-    params.require(:order).permit(:payment_method, :status, :postal_code, :address, :shipping_cost, :total_amount)
+    params.require(:order).permit(:payment_method, :status, :postal_code, :address, :shipping_cost, :total_amount, :name)
   end
-  
 end
