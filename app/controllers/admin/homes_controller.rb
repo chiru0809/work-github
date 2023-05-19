69e05f75c87742bba921b691511d70ba4f1f65bd
EC2 Default User <ec2-user@ip-172-31-17-60.ap-northeast-1.compute.internal>
2023-05-19 14:11:41 +0000

    [Add]管理者トップページ

--- app/controllers/admin/homes_controller.rb
+++ app/controllers/admin/homes_controller.rb
@@ -1,4 +1,6 @@
 class Admin::HomesController < ApplicationController
   def top
+    @orders = Order.all.page(params[:page]).per(10)
+    @order_amount = 0
   end
 end
--- app/models/order.rb
+++ app/models/order.rb
@@ -1,5 +1,6 @@
 class Order < ApplicationRecord
   has_many :order_details, dependent: :destroy
+  belongs_to :customer
   
   enum payment_method: { credit_card: 0, transfer: 1 }
   enum status: { payment_waiting: 0, payment_confirmation: 1, in_production: 2, preparing_delivery: 3, delivered: 4}
