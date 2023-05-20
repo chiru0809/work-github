c2b945acc359d1919ac386454d47955a2864759e
EC2 Default User <ec2-user@ip-172-31-17-60.ap-northeast-1.compute.internal>
2023-05-20 13:27:34 +0000

    [Add]管理者注文履歴詳細画面

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -2,4 +2,19 @@ class Admin::OrdersController < ApplicationController
   def show
     @order = Order.find(params[:id])
   end
+  
+  def update
+    @order = Order.find(params[:id])
+    if @order.update(order_params)
+      redirect_to admin_order_path(@order.id)
+    else
+      render :show
+    end
+  end
+  
+  private
+  
+  def order_params
+    params.require(:order).permit(:status)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -26,7 +26,7 @@ Rails.application.routes.draw do
     resources :genres, only: [:index, :create, :edit, :update]
     resources :items, only: [:index, :new, :create, :show, :edit, :update]
     resources :customers, only: [:index, :show, :edit, :update]
-    resources :orders, only: [:show]
+    resources :orders, only: [:show, :update]
     resources :order_details, only: [:update]
   end
   
