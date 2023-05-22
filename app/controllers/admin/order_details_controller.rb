ed64b9172467cefe5222cbcbc815aaa978e9c2f6
EC2 Default User <ec2-user@ip-172-31-17-60.ap-northeast-1.compute.internal>
2023-05-22 11:40:10 +0000

    [Add]注文・製造ステータスプルダウン

--- app/controllers/admin/order_details_controller.rb
+++ app/controllers/admin/order_details_controller.rb
@@ -1,2 +1,19 @@
 class Admin::OrderDetailsController < ApplicationController
+  def update
+    @order = Order.find(params[:id])
+    @order_detail = OrderDetail.find(params[:id])
+    if @order_detail.update(order_detail_params)
+      @order.update(status: "in_production") if @order_detail.making_status == "in_production"
+      redirect_to admin_order_path(@order.id)
+    else
+      render :show
+    end
+  end
+  
+  
+  private
+  
+  def order_detail_params
+    params.require(:order_detail).permit(:making_status)
+  end
 end
--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -1,11 +1,15 @@
 class Admin::OrdersController < ApplicationController
   def show
     @order = Order.find(params[:id])
+    @order_details = OrderDetail.where(order_id: @order.id)
+    @total_price = 0
   end
   
   def update
     @order = Order.find(params[:id])
+    @order_details = @order.order_details
     if @order.update(order_params)
+      @order_details.update_all(making_status: "production_pending") if @order.status == "payment_confirmation"
       redirect_to admin_order_path(@order.id)
     else
       render :show
@@ -16,5 +20,9 @@ class Admin::OrdersController < ApplicationController
   
   def order_params
     params.require(:order).permit(:status)
+  end
+  
+  def order_detail_params
+    params.require(:order).permit(:making_status)
   end
 end
