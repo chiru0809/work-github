348bbc2eb7a06dbec4dabb3d1d32a7e8647b3b63
EC2 Default User <ec2-user@ip-172-31-17-60.ap-northeast-1.compute.internal>
2023-05-23 13:45:44 +0000

    [Add]注文・製造ステータス連携

--- app/controllers/admin/order_details_controller.rb
+++ app/controllers/admin/order_details_controller.rb
@@ -1,10 +1,18 @@
 class Admin::OrderDetailsController < ApplicationController
   def update
-    @order = Order.find(params[:id])
     @order_detail = OrderDetail.find(params[:id])
+    @order = Order.find(@order_detail.order.id)
+    @order_details = @order.order_details.all
+    is_updated = true
     if @order_detail.update(order_detail_params)
       @order.update(status: "in_production") if @order_detail.making_status == "in_production"
-      redirect_to admin_order_path(@order.id)
+      @order_details.each do |order_detail|
+        if order_detail.making_status != "production_complete"
+          is_updated = false
+        end
+      end
+      @order.update(status: "preparing_delivery") if is_updated
+      redirect_to admin_order_path(@order_detail.order_id)
     else
       render :show
     end
