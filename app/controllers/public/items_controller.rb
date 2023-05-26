61dc91a48521f5b383e4decc6eece8bd78f4d69a
EC2 Default User <ec2-user@ip-172-31-17-60.ap-northeast-1.compute.internal>
2023-05-26 05:41:12 +0000

    [Add]ジャンル検索機能

--- app/controllers/public/items_controller.rb
+++ app/controllers/public/items_controller.rb
@@ -1,7 +1,9 @@
 class Public::ItemsController < ApplicationController
   def index
-    @items = Item.all.page(params[:page]).per(8)
+    @items = params[:genre_id].present? ? Genre.find(params[:genre_id]).items.page(params[:page]).per(8) : Item.all.page(params[:page]).per(8)
     @genres = Genre.all
+    @genre = params[:genre_id].present? ? Genre.find(params[:genre_id]).name : "商品"
+    @count = params[:genre_id].present? ? Genre.find(params[:genre_id]).items.count : Item.all.count
   end
 
   def show
